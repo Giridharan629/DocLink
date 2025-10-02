f03223e71604b1ad4f928ef2858d509d67eab43a
Giridharan C <118421351+Giridharan629@users.noreply.github.com>
2025-10-02 23:37:37 +0530

    animations fixed

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -53,7 +53,7 @@ const NavBar = () => {
 
         <img className="w-6 md:hidden " onClick={()=>setShowMenu(true)} src={assets.menu_icon} alt="" />
         {/* ========== mobile menu========== */}
-        <div className={`${showMenu ? "fixed w-full" : "h-0 w-0"} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+        <div className={`fixed md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all w-full ${showMenu ? "translate-x-0" : "translate-x-full"}`}>
           <div className="flex items-center justify-between px-5 py-6 ">
             <img className="w-36" src={assets.logo} alt="" />
             <img className="w-7" onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
